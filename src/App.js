2d1122f31935f8c2afb4b07c31597f48df5c1b2f
Vincent <overlordnacen@gmail.com>
2018-12-26 18:08:02 +0800

    Finished Markdown Previewer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
+let marked = require("marked");
+marked.setOptions({
+  pedantic: false,
+  gfm: true,
+  tables: true,
+  breaks: true,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false,
+  xhtml: false
+});
 class App extends Component {
+  state = {
+    value: `Markdown Syntax
+# Heading
+## Sub Heading
+[Link to Google](https://www.google.com)
+
+    This is a codeblock
+> Blockquotes
+
+1. List item
+
+**Bold Text**
+
+![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")
+
+\`\`
+  <div>Code Here</div>
+\`\`
+
+\`\`\`
+// this is inline multi-line code:
+
+let helloWorld = "Hello World";
+console.log(helloWorld.split(" ").reverse().join(" "));
+
+\`\`\`
+`
+  };
+
+  onTextChange = event => {
+    this.setState({ value: event.target.value });
+  };
   render() {
+    let { value } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div className="editor-container">
+          <h2 className="label">Editor</h2>
+          <textarea id="editor" onChange={this.onTextChange} value={value} />
+        </div>
+        <div className="preview-container">
+          <h2 className="label">Preview</h2>
+          <div
+            id="preview"
+            dangerouslySetInnerHTML={{ __html: marked(value) }}
+          />
+        </div>
       </div>
     );
   }
---
+++ src/component/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+const header = () => {
+  return (
+    <header className="header">
+      <h1>FreeCodeCamp Challenge Markdown Previewer</h1>
+    </header>
+  );
+};
+
+export default header;
